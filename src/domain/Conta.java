d4bffaaf96ba5a2c82e1ead9443c7c951a0b9de9
Matheus Leal Gomes da Silva <matheusleal@tre-pa.jus.br>
2022-08-17 10:42:00 -0300

    GET, boas práticas.

--- src/domain/Conta.java
+++ src/domain/Conta.java
@@ -6,6 +6,11 @@ public class Conta {
     private int conta;
     private Titular titular;
     private double saldo;
+    private double limite;
+
+    public Conta(double limite){
+        this.limite = limite;
+    }
 
     public void depositar(double valor){
         this.saldo=+valor;
@@ -18,9 +23,11 @@ public class Conta {
             return true;
         }
     }
-
     public void transferir(double valor, Conta contaDestino){
         sacar(valor);
         contaDestino.depositar(valor);
+    }
+    public double getSaldo() {
+        return saldo;
     }
 }
