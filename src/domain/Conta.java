f3a79b594ea3d4985920c10ba239d63cadbfdb69
Matheus Leal <matheuslealpa@gmail.com>
2022-08-26 12:06:30 -0300

    ajustes

--- src/domain/Conta.java
+++ src/domain/Conta.java
@@ -11,19 +11,19 @@ public abstract class Conta {
     private BigDecimal limite;
 
     public Conta(BigDecimal limite){
+        this.saldo.add(BigDecimal.ZERO);
         this.limite = limite;
     }
 
     public void deposita(BigDecimal valor){
-        BigDecimal deposito = valor;
-        saldo.add(deposito);
+        saldo.add(valor);
     }
 
     public void saca(BigDecimal valor){
-        if (this.saldo.add(this.limite).doubleValue() >= valor.doubleValue()){
+        if (this.saldo.doubleValue() >= valor.doubleValue()){
         	this.saldo.subtract(valor);
         }else{
-            throw new IllegalArgumentException("CLiente sem limite disponível");
+            throw new IllegalArgumentException("O valor solicitado não está disponível em sua conta");
         }
     }
     public void transferir(BigDecimal valor, Conta contaDestino){
